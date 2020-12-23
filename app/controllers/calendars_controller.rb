3b2ebd69db4a9be9a63a53f4691b99785640ca98
noe-kikuchi <noenoe888815@gmail.com>
2020-12-23 11:24:05 +0900

    issue6（曜日訂正）

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -25,23 +25,27 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
-    @week_days = []
-
+    @week_days = [] #プッシュしたときに入れる配列
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-
+    # プランのモデルから一致した条件（７日分のデータ）を取得
     7.times do |x|
-      today_plans = []
+      today_plans = []#後の作業からプッシュされた時に入れる配列を入れる
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
+        #プランの数だけ実行
+        #新しいプランをプッシュするときに、プランのデータがそれぞれの１週間分の日付に一致してれば当てはめる
       end
 
-      wday_num = Date.today.wday # wdayメソッドを用いて取得した数値
+      wday_num = Date.today.wday + x# wdayメソッドを用いて取得した数値
+      # Xは今日は０、明日分は１、明後日分は２・・・と増えていく
+      # 一週間分のデータを出したいifではその数字が７を超えたときの配列の数字を出す式になっている
       if wday_num >= 7  #「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
       days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans, :wday => wdays[wday_num]}
       @week_days.push(days)
+      # プッシュした時の配列を入れるところが２８行目
     end
 
   end
